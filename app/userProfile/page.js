ee843f06d36fe81bc8b1718361f3628b9c8a2e68
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-05-28 21:57:09 -0600

    Wrapping useSearchParams() in a suspense boundary

--- app/userProfile/page.js
+++ app/userProfile/page.js
@@ -8,22 +8,31 @@ import RecipeList from "../recipeList/page";
 import FilterPage from "../filterPage/page";
 import Filter from "../filter/page";
 
-
-export default function Page() {
-
+function ProfileContent() {
     const searchParams = useSearchParams();
     const userName = searchParams.get("userName");
 
     return (
+        <div>
+            <h1 className="text-3xl font-bold">Welcome {userName}!</h1>
+            <Data name={userName}/>
+            <hr />
+            <RecipeList name={userName}/>
+            {/* <FilterPage name={userName}/> */}
+            <Filter />
+            {/* <Link href="../filter" className="text-cyan-600 underline hover:text-cyan-300">Filter All</Link> */}
+        </div>
+    );
+}
+
+export default function Page() {
+
+    
+
+    return (
         <main style={{ maxWidth: 400, margin: "2rem auto" }}>
             <Suspense fallback={<p>Loading user profile...</p>}>
-                <h1 className="text-3xl font-bold">Welcome {userName}!</h1>
-                <Data name={userName}/>
-                <hr />
-                <RecipeList name={userName}/>
-                {/* <FilterPage name={userName}/> */}
-                <Filter />
-                {/* <Link href="../filter" className="text-cyan-600 underline hover:text-cyan-300">Filter All</Link> */}
+                <ProfileContent />
             </Suspense>
             <Link href="/" className="text-cyan-600 underline hover:text-cyan-300">Home</Link>
         </main>
